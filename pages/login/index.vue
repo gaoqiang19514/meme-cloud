4b36e7b057905308a9bd65b0a2bb691c9bebf6db
tomcat <gaoqiang19514@163.com>
2023-11-17 09:45:10 +0800

    feat: 添加登录页面

---
+++ pages/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view>
+    login
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    onLoad() {},
+  }
+</script>
+
+<style>
+
+</style>
