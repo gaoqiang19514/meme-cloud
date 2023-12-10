f6949e954dab734bc729e0f5b621d31860819dd8
tomcat <gaoqiang19514@163.com>
2023-12-10 18:49:32 +0800

    feat: 初始化请求通知权限

--- App.vue
+++ App.vue
@@ -1,40 +1,42 @@
 <script>
-  import {
-    accountStorage,
-  } from '@/util.js'
+import {
+  accountStorage,
+} from '@/util.js'
 
-  export default {
-    onLaunch: function() {
-      const username = accountStorage.get()
+export default {
+  onLaunch: function () {
+    const username = accountStorage.get()
+    
+    Notification.requestPermission()
 
-      if (!username) {
-        uni.reLaunch({
-          url: '/pages/login/index'
-        })
-        return;
-      }
-    },
-  }
+    if (!username) {
+      uni.reLaunch({
+        url: '/pages/login/index'
+      })
+      return;
+    }
+  },
+}
 </script>
 
 <style>
-  /*hyksk*/
-  @font-face {
-    font-family: customFont;
-    src: url('https://blink.obs.cn-east-3.myhuaweicloud.com/615093268039929856/1699342977175-720d2547-d26d-4a12-9f2b-567618be8a53.ttf');
-  }
+/*hyksk*/
+@font-face {
+  font-family: customFont;
+  src: url('https://blink.obs.cn-east-3.myhuaweicloud.com/615093268039929856/1699342977175-720d2547-d26d-4a12-9f2b-567618be8a53.ttf');
+}
 
-  body {
-    font-family: customFont !important;
-  }
+body {
+  font-family: customFont !important;
+}
 
-  ul {
-    padding: 0;
-    list-style-type: none;
-  }
+ul {
+  padding: 0;
+  list-style-type: none;
+}
 
-  img {
-    width: 100%;
-    display: block;
-  }
+img {
+  width: 100%;
+  display: block;
+}
 </style>
