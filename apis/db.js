82ef27415a6b1297517a2f2d1fcfe19e4535d6c8
tomcat <gaoqiang19514@163.com>
2023-12-12 15:43:34 +0800

    refactor: 添加注释

--- apis/db.js
+++ apis/db.js
@@ -1,5 +1,6 @@
 const isProduction = window.location.href.includes('static-mp-5fa4a496-0aa2-45a9-b89c-4054536ad7e7');
 
+// 本地返回测试环境的数据库 线上返回生产环境的数据库
 const db = isProduction
   ? uniCloud.database()
   : uniCloud.database({
