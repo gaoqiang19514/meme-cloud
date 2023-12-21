49b46f1c9ab5935b75a69fc2961ab3b321642ac9
tomcat <gaoqiang19514@163.com>
2023-12-21 16:43:26 +0800

    refacotr: 完善云函数

--- uniCloud-aliyun/cloudfunctions/user/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/user/index.obj.js
@@ -6,17 +6,39 @@ const db = uniCloud.database({
 
 const userTable = db.collection('user');
 
+
+// get返回的结构
+// {
+//   "affectedDocs": 1,
+//   "data": [{
+//     "_id": "6583db5d6e5d2d7187ef6e6a",
+//     "username": "tomcat123",
+//     "password": "123@qq"
+//   }]
+// }
+
 module.exports = {
   list(params) {
+    const query = Object.keys(params).length ? params : {};
+    return userTable.where(query).get()
+  },
+  add() {
+    const httpInfo = this.getHttpInfo()
+    const body = JSON.parse(httpInfo.body);
+    return userTable.add(body);
+  },
+  login() {
+    const httpInfo = this.getHttpInfo()
+    const body = JSON.parse(httpInfo.body);
     const {
-      username
-    } = params;
-    
+      username,
+      password
+    } = body;
+
+    // 检查username和password
     return userTable.where({
       username,
-    }).get()
-  },
-  add(params) {
-    return userTable.add(params);
+      password
+    }).get();
   },
 }
