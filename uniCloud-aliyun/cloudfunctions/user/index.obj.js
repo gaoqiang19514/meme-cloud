17b9e9ba09dd37cf0b2f902fc23a525011a1c51b
tomcat <gaoqiang19514@163.com>
2023-12-22 15:33:12 +0800

    refactor: 接口添加注释

--- uniCloud-aliyun/cloudfunctions/user/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/user/index.obj.js
@@ -12,17 +12,76 @@ const userTable = db.collection('user');
 //   }]
 // }
 
+/**
+ * @typedef {Object} User
+ * @property {string} _id
+ * @property {string} username
+ * @property {string} password
+ * @property {string} phone
+ * @property {string} email
+ * @property {string[]} images
+ */
+
+/**
+ * @typedef {Object} ApiResponse
+ * @property {number} code
+ * @property {any} data
+ */
+
+/**
+ * 查询用户列表
+ * @param {Object} params
+ * @param {string} [params._id]
+ * @param {string} [params.username]
+ * @param {string} [params.password]
+ * @param {string} [params.phone]
+ * @param {string} [params.email]
+ * @returns {ApiResponse}
+ */
 function list(params) {
   return userTable.where(params).get();
 }
 
+/**
+ * 修改密码
+ * @param {Object} params
+ * @param {string} params.username
+ * @param {string} params.password
+ * @param {string} params.newPassword
+ * @returns {ApiResponse}
+ */
 async function updatePassword() {
-  const httpInfo = this.getHttpInfo();
-  const body = JSON.parse(httpInfo.body);
-
+  const body = JSON.parse(this.getHttpInfo());
   const { username, password, newPassword } = body;
 
-  // 检查username和password
+  if (!username) {
+    return {
+      code: -1,
+      data: '缺少用户名',
+    };
+  }
+
+  if (!password) {
+    return {
+      code: -1,
+      data: '缺少原始密码',
+    };
+  }
+
+  if (!newPassword) {
+    return {
+      code: -1,
+      data: '缺少新密码',
+    };
+  }
+
+  if (password === newPassword) {
+    return {
+      code: -1,
+      data: '新密码与原始密码一致',
+    };
+  }
+
   const res = await userTable
     .where({
       username,
@@ -31,26 +90,34 @@ async function updatePassword() {
     .get();
 
   if (res.data.length) {
-    const res = await userTable
+    await userTable
       .where({
-        username,
-        password,
+        _id: res.data[0]._id,
       })
       .update({
         password: newPassword,
       });
 
-    return res;
+    return {
+      code: 0,
+      data: '修改成功',
+    };
   }
 
   return {
-    data: '原始密码错误',
+    code: -1,
+    data: '账号或密码错误',
   };
 }
 
+/**
+ * 找回密码
+ * @param {Object} params
+ * @param {string} params.username
+ * @returns {ApiResponse}
+ */
 async function forgetPassword() {
-  const httpInfo = this.getHttpInfo();
-  const body = JSON.parse(httpInfo.body);
+  const body = JSON.parse(this.getHttpInfo());
   const { username } = body;
 
   // 获取账户信息
@@ -62,6 +129,7 @@ async function forgetPassword() {
 
   if (res.data.length) {
     return {
+      code: 0,
       data: '请去邮箱中重置密码',
     };
   }
@@ -71,28 +139,67 @@ async function forgetPassword() {
   // 如何验证用户注册时填写的邮箱呢？
 
   return {
+    code: -1,
     data: '账户不存在',
   };
 }
 
+/**
+ * 新增用户
+ * @param {Object} params
+ * @param {string} params.username
+ * @param {string} params.password
+ * @returns {ApiResponse}
+ */
 function add() {
-  const httpInfo = this.getHttpInfo();
-  const body = JSON.parse(httpInfo.body);
+  const body = JSON.parse(this.getHttpInfo());
+
+  if (!body.username) {
+    return {
+      code: -1,
+      data: '缺少用户名',
+    };
+  }
+
+  if (!body.password) {
+    return {
+      code: -1,
+      data: '缺少密码',
+    };
+  }
+
+  // TODO: 检查密码强度
+
   return userTable.add(body);
 }
 
+/**
+ * 登录
+ * @param {Object} params
+ * @param {string} params.username
+ * @param {string} params.password
+ * @returns {ApiResponse}
+ */
 async function login() {
-  const httpInfo = this.getHttpInfo();
-  const body = JSON.parse(httpInfo.body);
-  const { username, password } = body;
+  const body = JSON.parse(this.getHttpInfo());
 
-  // 检查username和password
-  const res = await userTable
-    .where({
-      username,
-      password,
-    })
-    .get();
+  if (!body.username) {
+    return {
+      code: -1,
+      data: '缺少用户名',
+    };
+  }
+
+  if (!body.password) {
+    return {
+      code: -1,
+      data: '缺少密码',
+    };
+  }
+
+  // TODO: 检查密码
+
+  const res = await userTable.where(body).get();
 
   if (res.data.length) {
     return {
